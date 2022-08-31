3e80e2052858b74848fd0855a642638932210ee9
Jaedson Silva <contacanal798@gmail.com>
2022-08-31 19:34:26 -0300

    adding waitRFIDCard function to read tag ID

--- poinsys.ino
+++ poinsys.ino
@@ -8,6 +8,8 @@ const int rstPin = 9;
 MFRC522 rfid(ssPin, rstPin);
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
+String waitRFIDCard();
+
 void setup() {
   lcd.init();
   lcd.backlight();
@@ -23,4 +25,18 @@ void setup() {
 
 void loop() {
 
+}
+
+String waitRFIDCard() {
+  while(!rfid.PICC_IsNewCardPresent() || !rfid.PICC_ReadCardSerial()) {
+    delay(50);
+  }
+
+  String uid = "";
+
+  for(byte i = 0; i < rfid.uid.size; i++) {
+    uid.concat(String(rfid.uid.uidByte[i], HEX));
+  }
+
+  return uid;
 }
