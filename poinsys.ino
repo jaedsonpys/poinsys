546549ed7dc09a233c2be4507d87f1ff5ee9c4e3
Jaedson Silva <contacanal798@gmail.com>
2022-08-31 19:27:09 -0300

    removing LCD display blink from setup

--- poinsys.ino
+++ poinsys.ino
@@ -11,14 +11,14 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);
 void setup() {
   lcd.init();
   lcd.backlight();
-  
-  lcd.print("Poinsys");
-  lcd.blink();
 
   SPI.begin();
   rfid.PCD_Init();
 
   Serial.begin(9600);
+
+  lcd.setCursor(4, 0);
+  lcd.print("Poinsys");
 }
 
 void loop() {
