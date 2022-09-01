3bd78f4a9a18bfa4529df2d1195eb8cf39e1c17f
Jaedson Silva <contacanal798@gmail.com>
2022-09-01 08:12:08 -0300

    sending current date on started arduino

--- poinsys.py
+++ poinsys.py
@@ -1,8 +1,17 @@
 import serial
 import cookiedb
 
+import datetime
 
-class Poinsys:
+
+class PoinSys:
     def __init__(self, serial_port: str) -> None:
         self.ser = serial.Serial(serial_port)
         self.cookiedb = cookiedb.CookieDB()
+
+        signal = self.ser.readline().decode()
+        signal = signal.replace('\r\n', '')
+
+        if signal == 'started':
+            date = datetime.datetime.now()
+            self.ser.write(date.strftime('%d/%m/%Y').encode())
