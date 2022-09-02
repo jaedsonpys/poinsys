11916146c8262ac92694b817ebe2db0274278c6c
Jaedson Silva <contacanal798@gmail.com>
2022-09-02 18:58:53 -0300

    checking database config before serial port request

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-import cookiedb
+import os
+
+import serial
+
 from management.poinsys import PoinSys
 
 
-def main():
+def main():    
+    if not os.path.isfile('poinsys.cookiedb'):
+        print('\033[31mPlease make database setup first\033[m')
+        return None
+
     serial_port = input('Serial port: ').strip()
-    
+
     try:
         PoinSys(serial_port)
-    except cookiedb.exceptions.DatabaseNotFoundError:
-        print('\033[31mPlease make database setup first\033[m')
+    except serial.SerialException:
+        print('\033[31mInvalid serial port. Try again.\033[m')
 
 
 main()
